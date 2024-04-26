2286769521bd42f7a6d5ce22351a8ab331c879fc
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-04-26 17:53:37 +0900

    dsfdsf

---
+++ javabasic/src/javabasic/LoofExer.java
@@ -0,0 +1,158 @@
+package javabasic;
+
+public class LoofExer {
+	
+	public static void main(String[] args) {
+		
+		// 별찍기 1
+		//*
+		//**
+		//***
+		//****
+		//*****
+		for (int i=0; i<5; i++ ) {
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		// 별찍기 2
+		//        *
+		//      **
+		//    ***
+		//  ****
+		//*****
+		for (int i=0; i<5; i++ ) {
+			for (int k=0; k<4-i; k++) {
+				System.out.print("o");
+			}
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}		
+		
+		System.out.println();
+		
+		// 별찍기 3
+		//*****		
+		//****
+		//***
+		//**
+		//*
+		for (int i=5; i>0; i-- ) {
+			for (int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		// 별찍기 4
+		// *****
+		// *     *
+		// *     *
+		// *     *
+		// *****		
+		for (int i=0; i<5; i++ ) {
+			for (int j=0; j<5; j++) {
+				if (i%4==0 || j%4==0) {
+					System.out.print("*");
+				} else {
+					System.out.print("o");
+				}
+			}
+			System.out.println();
+		}
+		
+		// 별찍기 5
+		// *****
+		// **  **
+		// *  *  *
+		// **  **
+		// *****			
+//		for (int i=0; i<5; i++ ) {
+//			for (int j=0; j<5; j++) {
+//				//if (i%4==0 || j%4==0 || i==j || i+j==4) {
+//				if (i%4==0 || j%4==0 || i==j || i*j==3) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print("o");
+//				}
+//			}
+//			System.out.println();
+//		}	
+		
+//		System.out.println();
+//		
+//		int keynum = 12;
+//		for (int i=0; i<keynum+1; i++ ) {
+//			for (int j=0; j<keynum+1; j++) {
+//				if (i%keynum==0 || j%keynum==0) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print("o");
+//				}
+//			}
+//			System.out.println();
+//		}	
+		
+		// 별찍기 6 (Z)
+		//*****
+		//ooo*o
+		//oo*oo
+		//o*ooo
+		//*****
+//		System.out.println();
+//		
+//		int keynum = 12;
+//		for (int i=0; i<keynum+1; i++ ) {
+//			for (int j=0; j<keynum+1; j++) {
+//				if (i%keynum==0 || i+j==keynum) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print("o");
+//				}
+//			}
+//			System.out.println();
+//		}			
+		
+		
+		// 별찍기 7 (H)
+		//*ooo*
+		//*ooo*
+		//*****
+		//*ooo*
+		//*ooo*
+		System.out.println();
+		
+		int keynum = 8;
+		for (int i=0; i<keynum+1; i++ ) {
+			for (int j=0; j<keynum+1; j++) {
+				if (j%keynum==0 || i==keynum/2) {
+					System.out.print("*");
+				} else {
+					System.out.print("o");
+				}
+			}
+			System.out.println();
+		}			
+		
+		// 별찍기 8 (다이아몬드)
+		//oo*oo
+		//o***o
+		//*****
+		//o***o
+		//oo*oo		
+				
+	} // main
+
+} // class
+
+
+
+
--- javabasic/src/javabasic/Roof.java
+++ javabasic/src/javabasic/Roof.java
@@ -1,5 +1,8 @@
 package javabasic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Roof {
 
 	public static void main(String[] args) {
@@ -40,6 +43,73 @@ public class Roof {
 //			}
 //		}
 		
+		// enhanced for
+//		int[] intArray = {10, 20, 30, 40};
+//		for (int i : intArray) {
+//			System.out.println(i);
+//		}
+//		for (int i=0; i<intArray.length; i++) {
+//			System.out.println(intArray[i]);
+//		}
+//		
+//		List<String> strList = new ArrayList<String>();
+//		strList.add("홍길동");
+//		strList.add("강감찬");
+//		strList.add("유관순");
+//		for (String str : strList) {
+//			System.out.println(str);
+//		}
+//		for (int i=0; i<strList.size(); i++) {
+//			System.out.println(strList.get(i));
+//		}
+		
+//		int k = 10;
+//		while (k<100) {
+//			System.out.println(k);
+//			k++;
+//		}
+		
+//		int l = 1;
+//		while (l<100) {
+//			if (l==50) break;
+//			l++;
+//			if (l%2==0) continue;
+//			System.out.println(l);
+//		}
+		
+		// 실습 1) while문을 사용하여 10부터 100까지의 정수 중에서 3의 배수만 출력
+//		int i = 10;
+//		while (i<101) {
+//			if (i%3==0) {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}
+		
+		// 실습 2) while문을 사용하여 10부터 100까지의 정수 중에서 3의 배수이지만
+		//           6의 배수는 아닌 수들만 출력
+//		int i = 10;		
+//		while (i<101) {
+//			if (i%3==0 && i%6!=0) {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}
+		
+		// 실습 3) while문을 사용해서 10부터 100까지의 정수 중에서 3의 배수 아닌것들 출력
+//		int i = 9;
+//		while (i<101) {
+//			i++;
+//			if (i%3==0) continue;
+//			System.out.println(i);
+//		}		
+		
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i<10);
+		
 	} // main
 
 	static boolean isPrime(int n) {
@@ -54,3 +124,10 @@ public class Roof {
 	}
 
 } // class
+
+
+
+
+
+
+
